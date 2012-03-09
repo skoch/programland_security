cb073452fddc5c060cf7de4e61e9794478923107
Stephen Koch <s.koch@bigspaceship.com>
2012-03-09 16:02:53 -0500

    user creation, changed name of Robots

--- dev/bin/includes/php/createUser.php
+++ dev/bin/includes/php/createUser.php
@@ -1,24 +1,91 @@
 <?php
+	
+	$mongo  = new Mongo();
+	$db = $mongo->big_spaceship;
+	$staff_collection = $db->staff;
 
-	if(!empty($_POST[coder]))
+	if( isset( $_POST['full_name'] ) )
 	{
-		include("basicMySqlConnect.php");
+		$user = array(
+			"full_name" 	=> $_POST['full_name'],
+			"title" 		=> $_POST['title'],
+			"email" 		=> $_POST['email'],
+			"team" 			=> $_POST['team'],
+			"discipline" 	=> $_POST['discipline'],
+			"password" 		=> $_POST['password'],
+			"status" 		=> $_POST['status']
+		);
 
-		$result = mysql_query("INSERT INTO coder_status(coder,username,password,status) VALUES ('$_POST[coder]', '$_POST[username]', '$_POST[password]', '0')");
-		if($result) echo "You're in there.";
-		else echo "Error.";
-	}
-	else
+		if( $staff_collection->insert( $user ) )
+		{
+			echo "User creation successful.<br>";
+		}else
+		{
+			echo "There was a problem.<br>";
+		}
+		echo "<a href='createUser.php'>back</a>";
+
+	}else
 	{
+		$teamsHTML = '';
+		$disciplineHTML = '';
+		$statusHTML = '';
+		$teams = array(
+			"Squid Republic",
+			"The Special Bears",
+			"Cheapies Playhaus",
+			"The Stephanies",
+			"y [it's spanish]",
+			"Milkshake Enterprise",
+			"Core"
+		);
+		foreach( $teams as $key=>$option )
+		{
+			$teamsHTML .= '<option value="' . $option . '" ' . ($key == 0 ? 'selected="true">' : '>') . $option . '</option>\n';
+		}
+
+		$disciplines = array(
+			"producers",
+			"strategy",
+			"technology",
+			"designers",
+			"core"
+		);
+		foreach( $disciplines as $key=>$option )
+		{
+			$disciplineHTML .= "<option value='" . $option . "' " . ($key == 0 ? "selected='true'>" : ">") . $option . "</option>\n";
+		}
+
+		$status_text = array(
+			"Free",
+			"A Little Workload",
+			"Pretty Crazy",
+			"Insane",
+			"Leave this person alone"
+		);
+		foreach( $status_text as $key=>$option )
+		{
+			$statusHTML .= "<option value='" . $key . "' " . ($key == 0 ? "selected='true'>" : ">") . $option . "</option>\n";
+		}
+
 		$eof =
 <<<EOF
 		<html>
 			<body>
 				<form action="createUser.php" method="POST">
-					Your Coder Name: <input type="text" name="coder"> <br />
-					Your User Name: <input type="text" name="username"><br />
-					Your PW: <input type="password" name="password"><br />
-						
+					Full Name: <input type="text" name="full_name"> <br>
+					Title: <input type="text" name="title"><br>
+					Email: <input type="text" name="email"><br>
+					Password: <input type="password" name="password"><br>
+					Team: <select name="team">
+								$teamsHTML
+							</select><br>
+					Discipline: <select name="discipline">
+								$disciplineHTML
+							</select><br>
+					Status: <select name="status">
+								$statusHTML
+							</select>
 					<input type="submit">
 				</form>
 			</body>
