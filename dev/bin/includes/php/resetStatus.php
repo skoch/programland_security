996cf0cc6821e51a949813a07cf6aba303571e56
Stephen Koch <s.koch@bigspaceship.com>
2012-03-21 16:09:29 -0400

    added a reset script

---
+++ dev/bin/includes/php/resetStatus.php
@@ -0,0 +1,27 @@
+<?php
+
+	$mongo  = new Mongo();
+	$db = $mongo->big_spaceship;
+	$staff_collection = $db->staff;
+
+	// $status = $_POST['status'];
+	$status = 0;
+	$cursor = $staff_collection->find();
+	while( $cursor->hasNext() )
+	{
+		$user = $cursor->getNext();
+
+		$staff_collection->update(
+			array(
+				"full_name" => $user['full_name']
+			),
+			array( '$set' => array(
+					'status' => $status
+				)
+			)
+		);
+	}
+
+	echo "done resetting all user statuses to: " . $status;
+
+?>
